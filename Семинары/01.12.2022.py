c333c2af1df042a02215e1996a3e15febe8d1c4a
bonew1988 <bonew@bonew.ru>
2022-12-02 00:04:51 +0300

    test 1.12.22v2

--- Семинары/01.12.2022.py
+++ Семинары/01.12.2022.py
@@ -8,55 +8,55 @@
 # 1) с помощью математических формул нахождения корней квадратного уравнения (D = b^2-4ac, x1 = (-b+/- sqtr(D))/a)
 # 2) с помощью дополнительных библиотек Python(sympy, scipy)(Дополнительно)
 
-# import cmath
+# # import cmath
 
-a = float(input('Input A: '))
-b = float(input('Input B: '))
-c = float(input('Input C: '))
+# a = float(input('Input A: '))
+# b = float(input('Input B: '))
+# c = float(input('Input C: '))
 
-d = (b**2) - (4*a*c)
-if d > 0:
-    x1 = (-b-cmath.sqrt(d))/(2*a)
-    x2 = (-b+cmath.sqrt(d))/(2*a)
-    print(x1, x2)
-elif d == 0:
-    x = -b / (2 * a)
-    print(x)
-else:
-    print('Корней нет')
-
-
-# второвй вариант решение через библиотеку
-# import sympy
-
-# x = sympy.Symbol('x')
-# a = input('Input eq:')
-# solution = sympy.solve(a, x)
-# print(solution)
+# d = (b**2) - (4*a*c)
+# if d > 0:
+#     x1 = (-b-cmath.sqrt(d))/(2*a)
+#     x2 = (-b+cmath.sqrt(d))/(2*a)
+#     print(x1, x2)
+# elif d == 0:
+#     x = -b / (2 * a)
+#     print(x)
+# else:
+#     print('Корней нет')
 
 
+# # второвй вариант решение через библиотеку
+# # import sympy
+
+# # x = sympy.Symbol('x')
+# # a = input('Input eq:')
+# # solution = sympy.solve(a, x)
+# # print(solution)
 
 
-# 3. Задайте два числа. Напишите программу, которая найдёт НОК (наименьшее общее кратное) этих двух чисел.
-# Пример:
-# 4, 5 -> 20
-# 7,10 -> 70
 
 
-def nok(x, y):
-    if x > y:
-        max = x
-    else:
-        max = y
-    while(True):
-        if (max % x == 0) and (max % y == 0):
-            i = max
-            break
-        max += 1
-    return i
-num1 = int(input("Enter first digit: "))
-num2 = int(input("Enter second digit: "))
-print(nok(num1, num2))
+# # 3. Задайте два числа. Напишите программу, которая найдёт НОК (наименьшее общее кратное) этих двух чисел.
+# # Пример:
+# # 4, 5 -> 20
+# # 7,10 -> 70
+
+
+# def nok(x, y):
+#     if x > y:
+#         max = x
+#     else:
+#         max = y
+#     while(True):
+#         if (max % x == 0) and (max % y == 0):
+#             i = max
+#             break
+#         max += 1
+#     return i
+# num1 = int(input("Enter first digit: "))
+# num2 = int(input("Enter second digit: "))
+# print(nok(num1, num2))
 
 # второй вариант решения
 
