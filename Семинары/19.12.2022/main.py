861c7a4e7e902061b041b19246d168379b695436
bonew <bonew@bonew.ru>
2022-12-19 21:41:34 +0300

    add seminar 19.12.2022

---
+++ Семинары/19.12.2022/main.py
@@ -0,0 +1,20 @@
+# Реализовать для телеграм бота:
+# Напишите программу, удаляющую из текста все слова, содержащие "абв"
+
+
+import os
+os.system('clear')
+
+import telebot
+bot = telebot.TeleBot("5846844230:AAEvAYzZR1ms5RWuIF7Dxs7K7PP3nEcmpYw")
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+	bot.reply_to(message, "Howdy, how are you doing?")
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    lst = [i for i in message.text.split() if 'абв' not in i]
+    bot.reply_to(message, ' '.join(lst))
+
+bot.infinity_polling()
